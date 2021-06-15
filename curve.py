9c7c761bd20997e3490ff12e5466c6e481469afb
Justsneak7 <71436825+Justsneak7@users.noreply.github.com>
2021-06-15 20:01:28 +0530

    Add files via upload

--- curve.py
+++ curve.py
@@ -2,6 +2,6 @@ import pandas as pd
 import plotly.express as px
 import plotly.figure_factory as ff
 
-df=pd.read_csv("data.csv")
+df=pd.read_csv("graph/data.csv")
 fig=ff.create_distplot([df["Mobile Brand"].to_list()], ["Brand Of Mobile"], show_hist=False)
 fig.show()
--- rating.py
+++ rating.py
@@ -2,6 +2,6 @@ import pandas as pd
 import plotly.express as px
 import plotly.figure_factory as ff
 
-df=pd.read_csv("data.csv")
+df=pd.read_csv("graph/data.csv")
 fig=ff.create_distplot([df["Avg Rating"].to_list()], ["Rating"], show_hist=False)
 fig.show()
